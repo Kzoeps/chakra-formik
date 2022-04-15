455456047094ea509a85d88c58190f29f7948b85
kzoeps <kzoepa@gmail.com>
2022-04-15 17:39:29 +0600

    :wrench: (jest.config) add jest config and configure for jsx

--- src/simple-field/index.test.tsx
+++ src/simple-field/index.test.tsx
@@ -1,3 +1,19 @@
-test("1 + 1 is equal to 2", () => {
+import {Form, Formik} from 'formik';
+import React from 'react';
+import SimpleField from './index';
+import {render} from '@testing-library/react';
+
+const InputContainer = () => {
+	return (
+		<Formik initialValues={{field: 'init value'}} onSubmit={() => {}}>
+			<Form>
+				<SimpleField name={'field'}/>
+			</Form>
+		</Formik>
+	);
+};
+
+test('1 + 1 is equal to 2', () => {
+	const {getByTestId} = render(<InputContainer/>);
 	expect(1 + 1).toBe(2);
-})
+});
