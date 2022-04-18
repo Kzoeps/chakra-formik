19fb56d620a9226bbac8311784c19b369c75b67d
kzoeps <kzoepa@gmail.com>
2022-04-18 22:17:12 +0600

    :white_check_mark: (form control) added test for correct render of lable

--- src/form-control/index.test.tsx
+++ src/form-control/index.test.tsx
@@ -4,12 +4,16 @@ import FormItem from './index';
 import SimpleField from '../simple-field';
 import {render} from '@testing-library/react';
 
+interface FormContainerProps {
+	initValue?: string;
+	label?: string;
+}
 
-const FormContainer = ({initValue = ''}: {initValue?: string}) => {
+const FormContainer = ({initValue = '', label = undefined}: FormContainerProps) => {
 	return (
 		<Formik initialValues={{field: initValue}} onSubmit={() => {}}>
 			<Form>
-				<FormItem data-testid="form-item" name={'field'}>
+				<FormItem data-testid="form-item" label={label} name={'field'}>
 					<SimpleField name={'field'}/>
 				</FormItem>
 			</Form>
@@ -21,3 +25,8 @@ test('FormItem renders correctly', () => {
 	const {getByTestId} = render(<FormContainer/>);
 	expect(getByTestId('form-item')).toBeDefined();
 });
+
+test('FormItem renders label when provided', () => {
+	const {getByTestId} = render(<FormContainer label="name"/>);
+	expect(getByTestId('label')).toBeDefined();
+})
