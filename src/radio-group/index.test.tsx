b44618ee2fc8c8f3cc7f445fc8d3668955084c15
kzoeps <kzoepa@gmail.com>
2022-04-20 15:25:23 +0600

    :white_check_mark: (radio-group) should render children correctly

--- src/radio-group/index.test.tsx
+++ src/radio-group/index.test.tsx
@@ -7,9 +7,9 @@ import {render} from '@testing-library/react';
 const RadioContainer = () => {
 	return <>
 		<RadioField data-testid="radio" name={'field'}>
-			<Radio value="1">One</Radio>
-			<Radio value="2">Two</Radio>
-			<Radio value="3">Three</Radio>
+			<Radio value="1" data-testid="radio-box">One</Radio>
+			<Radio value="2" data-testid="radio-box">Two</Radio>
+			<Radio value="3" data-testid="radio-box">Three</Radio>
 		</RadioField>
 	</>
 }
@@ -18,4 +18,9 @@ const RadioContainer = () => {
 test('should render radio container', () => {
 	const {queryByTestId} = render(<RadioContainer/>);
 	expect(queryByTestId('radio')).toBeInTheDocument();
+})
+
+test('should render radio boxes', () => {
+	const {queryAllByTestId} = render(<RadioContainer/>);
+	expect(queryAllByTestId('radio-box')).toHaveLength(3);
 })
